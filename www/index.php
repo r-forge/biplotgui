37b7929d88e56f822c9573463698f2f05ee3f22f
amlg (Anthony la Grange) <anthonylagrange@gmail.com>
2008-08-02 12:27:45 +0000

    Changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biplotgui@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,24 +1,15 @@
-
 <!-- This is the project specific website template -->
 <!-- It can be changed as liked or replaced by other content -->
 
-<?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
 
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<title>BiplotGUI: Interactive Biplots in R</title>
+	<link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
 <body>
@@ -26,26 +17,23 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <! --- R-Forge Logo --- >
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="/"><img src="http://r-forge.r-project.org/themes/rforge//images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
 
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<h2>Welcome to the BiplotGUI project!</h2>
 
+<p>BiplotGUI allows users to construct and interact with biplots in 2D or 3D. Observations are represented as points while variables are represented as calibrated biplot axes. Such biplots are multivariate analogues of ordinary scatter plots.
 <!-- end of project description -->
 
 <p> No content added. </p>
 
-<p> To download the package, click <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> To download the package, click <a href="http://r-forge.r-project.org/projects/biplotgui/"><strong>here</strong></a>. </p>
 
 </body>
+
 </html>
+
